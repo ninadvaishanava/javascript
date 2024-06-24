0a05596cb33b2f203688738735c5e91eb45e6146
ninad <vaishnavninad583@gmail.com>
2024-06-24 16:18:59 +0530

    all

---
+++ array.js
@@ -0,0 +1,10 @@
+// const marvelhero = ["thor", "ironman", "spiderman"];
+// const dchero = ["flash", "superman", "batman"];
+
+// const hero = marvelhero.concat(dchero);
+// console.log(hero);
+
+const array = [2, 3, 3, [2, 5, 4], 4];
+
+const solvearray = array.flat();
+console.log(solvearray);
